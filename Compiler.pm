b4768b99a03c313fd5d04ebff3c0850b33a4f1c2
Evgeny Martynov <evgeny@epochfail.com>
2013-09-12 05:06:07 +0000

    Factor out %cs

--- Compiler.pm
+++ Compiler.pm
@@ -4,6 +4,8 @@ use strict;
 use Data::Dumper;
 use Switch;
 
+our %cs;
+
 sub display {
   local $Data::Dumper::Terse = 1;
   $_ = Dumper(@_);
@@ -19,8 +21,6 @@ sub emit {
 }
 
 sub emit_newline {
-  my %cs = %{shift @_};
-
   print "\n" . ('  ' x $cs{depth});
 }
 
@@ -31,21 +31,18 @@ sub lookup_variable {
 }
 
 sub compile_comment {
-  my %cs = %{shift @_};
   my %node = %{shift @_};
 
   emit_string($node{value});
 }
 
 sub compile_number {
-  my %cs = %{shift @_};
   my %node = %{shift @_};
 
   emit($node{value});
 }
 
 sub compile_comma_sep_expr_onlist {
-  my %cs = %{shift @_};
   my @cld = @_;
 
   my $first = 1;
@@ -53,42 +50,38 @@ sub compile_comma_sep_expr_onlist {
     emit(",") unless $first;
     $first = 0;
 
-    compile_node(\%cs, $node_ref);
+    compile_node($node_ref);
   }
 }
 
 sub compile_function_call {
-  my %cs = %{shift @_};
   my $funcname = shift @_;
   my @args = @_;
 
   emit($funcname);
   emit("(");
-  compile_comma_sep_expr_onlist(\%cs, @args);
+  compile_comma_sep_expr_onlist(@args);
   emit(")");
 }
 
 sub compile_range {
-  my %cs = %{shift @_};
   my %node = %{shift @_};
 
   my $from_ref = shift @{$node{cld}};
   my $to_ref = shift @{$node{cld}};
 
-  compile_function_call(\%cs, 'xrange', $from_ref, $to_ref);
+  compile_function_call('xrange', $from_ref, $to_ref);
 }
 
 sub compile_stringify {
-  my %cs = %{shift @_};
   my %node = %{shift @_};
 
   emit("str(");
-  compile_node(\%cs, ${$node{cld}}[0]);
+  compile_node(${$node{cld}}[0]);
   emit(")");
 }
 
 sub compile_string {
-  my %cs = %{shift @_};
   my %node = %{shift @_};
 
   my $prefix = ($node{raw_string} ? 'r' : '');
@@ -99,30 +92,26 @@ sub compile_string {
 }
 
 sub compile_scalar {
-  my %cs = %{shift @_};
   my %node = %{shift @_};
 
   emit(lookup_variable($node{value}));
 }
 
 sub compile_print {
-  my %cs = %{shift @_};
   my %node = %{shift @_};
 
   emit('sys.stdout.write(');
-  compile_node(\%cs, @{$node{cld}}[0]);
+  compile_node(@{$node{cld}}[0]);
   emit(')');
 }
 
 sub compile_comma_sep_expr {
-  my %cs = %{shift @_};
   my %node = %{shift @_};
 
-  compile_comma_sep_expr_onlist(\%cs, @{$node{cld}});
+  compile_comma_sep_expr_onlist(@{$node{cld}});
 }
 
 sub compile_comma_sep_string_concat {
-  my %cs = %{shift @_};
   my %node = %{shift @_};
 
   # Listception. Hashception. Everythingception.
@@ -134,52 +123,48 @@ sub compile_comma_sep_string_concat {
     emit("+") unless $first;
     $first = 0;
 
-    compile_node(\%cs, $child);
+    compile_node($child);
   }
 }
 
 sub compile_assign {
-  my %cs = %{shift @_};
   my %node = %{shift @_};
 
   my $lvalue_node_ref = ${$node{cld}}[0];
   my $rvalue_node_ref = ${$node{cld}}[1];
 
-  compile_node(\%cs, $lvalue_node_ref);
+  compile_node($lvalue_node_ref);
   emit('=');
-  compile_node(\%cs, $rvalue_node_ref);
+  compile_node($rvalue_node_ref);
 }
 
 sub compile_parenthesise {
-  my %cs = %{shift @_};
   my %node = %{shift @_};
 
   emit('(');
   for my $node_ref (@{$node{cld}}) {
-    compile_node(\%cs, $node_ref);
+    compile_node($node_ref);
   }
   emit(')');
 }
 
 sub compile_binary_op_expr {
-  my %cs = %{shift @_};
   my %node = %{shift @_};
 
   my $lop_ref = shift @{$node{cld}};
   my $rop_ref = shift @{$node{cld}};
 
-  compile_node(\%cs, $lop_ref);
+  compile_node($lop_ref);
 
   while (defined($rop_ref)) {
     emit($node{operator});
-    compile_node(\%cs, $rop_ref);
+    compile_node($rop_ref);
 
     $rop_ref = shift @{$node{cld}};
   }
 }
 
 sub compile_if {
-  my %cs = %{shift @_};
   my %node = %{shift @_};
 
   my $cond_ref = shift @{$node{cld}};
@@ -187,25 +172,23 @@ sub compile_if {
   my $false_ref = shift @{$node{cld}};
 
   emit('if');
-  compile_node(\%cs, $cond_ref);
-  compile_node(\%cs, $true_ref);
-  compile_node(\%cs, $false_ref) if defined($false_ref);
+  compile_node($cond_ref);
+  compile_node($true_ref);
+  compile_node($false_ref) if defined($false_ref);
 }
 
 sub compile_while {
-  my %cs = %{shift @_};
   my %node = %{shift @_};
 
   my $cond_ref = shift @{$node{cld}};
   my $body_ref = shift @{$node{cld}};
 
   emit('while');
-  compile_node(\%cs, $cond_ref);
-  compile_node(\%cs, $body_ref);
+  compile_node($cond_ref);
+  compile_node($body_ref);
 }
 
 sub compile_foreach {
-  my %cs = %{shift @_};
   my %node = %{shift @_};
 
   my $iterator_ref = shift @{$node{cld}};
@@ -213,36 +196,34 @@ sub compile_foreach {
   my $body_ref = shift @{$node{cld}};
 
   emit('for');
-  compile_node(\%cs, $iterator_ref);
+  compile_node($iterator_ref);
   emit('in');
-  compile_node(\%cs, $range_ref);
-  compile_node(\%cs, $body_ref);
+  compile_node($range_ref);
+  compile_node($body_ref);
 }
 
 sub compile_body {
-  my %cs = %{shift @_};
   my %node = %{shift @_};
 
   $cs{depth}++;
 
   emit(':');
-  emit_newline(\%cs);
+  emit_newline();
 
   for my $node_ref (@{$node{cld}}) {
-    compile_node(\%cs, $node_ref);
-    emit_newline(\%cs);
+    compile_node($node_ref);
+    emit_newline();
   }
 
   $cs{depth}--;
 }
 
 sub compile_program {
-  my %cs = %{shift @_};
   my %node = %{shift @_};
 
   emit_string("#!/usr/bin/python2 -u");
   emit_string("import sys");
-  emit_newline(\%cs);
+  emit_newline();
 
   my @cld = @{$node{cld}};
 
@@ -252,42 +233,41 @@ sub compile_program {
   }
 
   for my $node_ref (@cld) {
-    compile_node(\%cs, $node_ref);
-    emit_newline(\%cs);
+    compile_node($node_ref);
+    emit_newline();
   }
 }
 
 sub compile_node {
-  my %cs = %{shift @_};
   my %node = %{shift @_};
 
   switch ($node{type}) {
-    case 'program'          { compile_program         (\%cs, \%node); }
-    case 'body'             { compile_body            (\%cs, \%node); }
+    case 'program'          { compile_program         (\%node); }
+    case 'body'             { compile_body            (\%node); }
 
-    case 'comment'          { compile_comment         (\%cs, \%node); }
-    case 'number'           { compile_number          (\%cs, \%node); }
-    case 'string'           { compile_string          (\%cs, \%node); }
-    case 'scalar'           { compile_scalar          (\%cs, \%node); }
-    case 'range'            { compile_range           (\%cs, \%node); }
+    case 'comment'          { compile_comment         (\%node); }
+    case 'number'           { compile_number          (\%node); }
+    case 'string'           { compile_string          (\%node); }
+    case 'scalar'           { compile_scalar          (\%node); }
+    case 'range'            { compile_range           (\%node); }
 
-    case 'assign'           { compile_assign          (\%cs, \%node); }
-    case 'add_expr'         { compile_binary_op_expr  (\%cs, \%node); }
-    case 'mul_expr'         { compile_binary_op_expr  (\%cs, \%node); }
-    case 'comparison'       { compile_binary_op_expr  (\%cs, \%node); }
-    case 'comma_sep_expr'   { compile_comma_sep_expr  (\%cs, \%node); }
+    case 'assign'           { compile_assign          (\%node); }
+    case 'add_expr'         { compile_binary_op_expr  (\%node); }
+    case 'mul_expr'         { compile_binary_op_expr  (\%node); }
+    case 'comparison'       { compile_binary_op_expr  (\%node); }
+    case 'comma_sep_expr'   { compile_comma_sep_expr  (\%node); }
 
-    case 'print_expr'       { compile_print           (\%cs, \%node); }
+    case 'print_expr'       { compile_print           (\%node); }
 
-    case 'if_expr'          { compile_if              (\%cs, \%node); }
-    case 'while_expr'       { compile_while           (\%cs, \%node); }
-    case 'foreach_expr'     { compile_foreach         (\%cs, \%node); }
+    case 'if_expr'          { compile_if              (\%node); }
+    case 'while_expr'       { compile_while           (\%node); }
+    case 'foreach_expr'     { compile_foreach         (\%node); }
 
-    case 'parenthesise'     { compile_parenthesise    (\%cs, \%node); }
+    case 'parenthesise'     { compile_parenthesise    (\%node); }
 
     # I hate perl at times. More so than the other times when I want to kill it.
-    case 'comma_sep_string_concat' { compile_comma_sep_string_concat(\%cs, \%node); }
-    case 'stringify'        { compile_stringify       (\%cs, \%node); }
+    case 'comma_sep_string_concat' { compile_comma_sep_string_concat(\%node); }
+    case 'stringify'        { compile_stringify       (\%node); }
 
     default           {
       print "What are you doing? Got this: ";
@@ -299,11 +279,11 @@ sub compile_node {
 
 sub compile {
   my $ast_ref = shift;
-  my %compile_state = (
+  %cs = (
     'depth' => 0,
   );
 
-  compile_node(\%compile_state, $ast_ref);
+  compile_node($ast_ref);
   print "## Compiled!\n";
 }
 
