447eae78d83bc4511122b0b41c25c8bbc29db2bb
Evgeny Martynov <evgeny@epochfail.com>
2013-10-01 04:15:22 +0000

    Add bitwise tests

---
+++ tests/bitwise.pl
@@ -0,0 +1,9 @@
+#!/usr/bin/perl
+
+print 'bitwise and: 12 & 6 gives  4: ', 12 & 6, "\n";
+print 'bitwise  or: 12 | 6 gives 14: ', 12 | 6, "\n";
+print 'bitwise xor: 12 ^ 6 gives 10: ', 12 ^ 6, "\n";
+print 'shifting:', "\n";
+print '  7 << 2 gives 28: ', 7 << 2, "\n";
+print '  7 >> 2 gives 1: ',  7 >> 2, "\n";
+print 'not: ~15: ', ~15, "\n";
