6fe5d219c41042a106819f16f5be857d73202ab1
Evgeny Martynov <evgeny@epochfail.com>
2013-10-01 05:37:19 +0000

    Clean up Compiler

--- Compiler.pm
+++ Compiler.pm
@@ -57,18 +57,16 @@ sub lookup_variable {
 }
 
 sub compile_comment {
-  emit_node_value(shift @_);
+  emit_node_value(shift);
 }
 
 sub compile_number {
-  emit_node_value(shift @_);
+  emit_node_value(shift);
 }
 
 sub compile_comma_sep_expr_onlist {
-  my @cld = @_;
-
   my $first = 1;
-  for my $node_ref (@cld) {
+  for my $node_ref (@_) {
     emit_token(",") unless $first;
     $first = 0;
 
@@ -77,8 +75,7 @@ sub compile_comma_sep_expr_onlist {
 }
 
 sub compile_function_call {
-  my $funcname = shift @_;
-  my @args = @_;
+  my ($funcname, @args) = @_;
 
   emit_identifier($funcname);
   emit_token("(");
@@ -87,22 +84,16 @@ sub compile_function_call {
 }
 
 sub compile_range {
-  my %node = %{shift @_};
-
-  my $from_ref = shift @{$node{cld}};
-  my $to_ref = shift @{$node{cld}};
+  my $node = shift;
+  my ($from_ref, $to_ref) = @{$node->{cld}};
 
   compile_function_call('xrange', $from_ref, $to_ref);
 }
 
 sub compile_comma {
   my $node = shift;
-  my @cld = @{$node->{cld}};
-  # my $brackets = $#cld > 1;
 
-  # emit_token('[') if $brackets;
-
-  foreach my $child (@cld) {
+  foreach my $child (@{$node->{cld}}) {
     given ($child->{type}) {
       when ('comma') {
         emit_token(',');
@@ -113,8 +104,6 @@ sub compile_comma {
       }
     }
   }
-
-  # emit_token(']') if $brackets;
 }
 
 sub compile_incdec {
@@ -123,90 +112,45 @@ sub compile_incdec {
 }
 
 sub compile_stringify {
-  my %node = %{shift @_};
-
-  compile_function_call('str', ${$node{cld}}[0]);
-}
-
-sub massage_string_concat_under_print_context {
-  my $current_node_ref = shift;   # Need the ref to strip newlines
-  my %current_node = %{$current_node_ref};
-  my $cld_ref = $current_node{cld};
-
-  my $eol_please = 0;
-  my $kill_me_please = 0;
-
-  given ($current_node{type}) {
-    when ('comma_sep_string_concat') {
-      my $rightmost_ref = ${$cld_ref}[$#{$cld_ref}];
-      ($eol_please, $kill_me_please) =
-          massage_string_concat_under_print_context($rightmost_ref);
-
-      # Don't want empty strings hanging around.
-      if ($kill_me_please) {
-        pop ${$current_node_ref}{cld};
-
-        # Or empty concats
-        $kill_me_please = 0 unless $#{$cld_ref};
-      }
-    }
-
-    when ('string') {
-      if ($current_node{eol}) {
-        ${$current_node_ref}{value} =~ s/\\n$//;
-        if (${$current_node_ref}{value} eq '') {
-          $kill_me_please = 1;
-        }
-
-        $eol_please = 1;
-      }
-    }
-  }
-
-  return ($eol_please, $kill_me_please);
+  my $node = shift;
+  compile_function_call('str', $node->{cld}->[0]);
 }
 
 sub compile_string {
-  my %node = %{shift @_};
+  my $node = shift;
 
-  my $prefix = ($node{raw_string} ? 'r' : '');
-  my $escaped = $node{value};
+  my $prefix = ($node->{raw_string} ? 'r' : '');
+  my $escaped = $node->{value};
   $escaped =~ s/'/\\'/g;
 
   emit_identifier("$prefix'$escaped'");
 }
 
 sub compile_scalar {
-  my %node = %{shift @_};
-
-  emit_identifier(lookup_variable($node{value}));
+  my $node = shift;
+  emit_identifier(lookup_variable($node->{value}));
 }
 
 sub compile_call {
-  my %node = %{shift @_};
+  my $node = shift;
 
-  my $func = $node{func};
+  my $func = $node->{func};
   emit_identifier($func);
   emit_token("(");
-  compile_comma($node{cld}->[0]);
+  compile_comma($node->{cld}->[0]);
   emit_token(")");
 }
 
 sub compile_comma_sep_expr {
-  my %node = %{shift @_};
-
-  compile_comma_sep_expr_onlist(@{$node{cld}});
+  my $node = shift;
+  compile_comma_sep_expr_onlist(@{$node->{cld}});
 }
 
 sub compile_comma_sep_string_concat {
-  my %node = %{shift @_};
+  my $node = shift;
 
-  # Listception. Hashception. Everythingception.
-  # ...contraception?
-  my @cld = @{$node{cld}};
   my $first = 1;
-
-  foreach my $child (@cld) {
+  foreach my $child (@{$node->{cld}}) {
     emit_token("+") unless $first;
     $first = 0;
 
@@ -215,39 +159,28 @@ sub compile_comma_sep_string_concat {
 }
 
 sub compile_assignment {
-  my %node = %{shift @_};
+  my $node = shift;
+  my ($lvalue_ref, $rvalue_ref) = @{$node->{cld}};
 
-  my $lvalue_node_ref = ${$node{cld}}[0];
-  my $rvalue_node_ref = ${$node{cld}}[1];
-
-  compile_node($lvalue_node_ref);
-
-  emit_token(
-    (defined $node{operator}
-        ? $node{operator}
-        : '')
-    . '='
-  );
-
-  compile_node($rvalue_node_ref);
+  compile_node($lvalue_ref);
+  emit_token(($node->{operator} || '') . '=');
+  compile_node($rvalue_ref);
 }
 
 sub compile_parenthesise {
-  my %node = %{shift @_};
+  my $node = shift;
 
   emit_token('(');
-  for my $node_ref (@{$node{cld}}) {
-    compile_node($node_ref);
+  for my $child (@{$node->{cld}}) {
+    compile_node($child);
   }
   emit_token(')');
 }
 
 sub compile_foldl {
   my $node = shift @_;
-  my @cld = @{$node->{cld}};
 
-  while (@cld) {
-    my $item = shift @cld;
+  foreach my $item (@{$node->{cld}}) {
     given ($item->{type}) {
       when ('operator') {
         emit_token($item->{operator});
@@ -261,7 +194,7 @@ sub compile_foldl {
 }
 
 sub compile_unary {
-  my $node = shift @_;
+  my $node = shift;
   my $ref = $node->{cld}->[0];
 
   emit_token($node->{operator}->{value});
@@ -271,43 +204,36 @@ sub compile_unary {
 }
 
 sub compile_binary_op_expr {
-  my %node = %{shift @_};
-
-  my $lop_ref = shift @{$node{cld}};
-  my $rop_ref = shift @{$node{cld}};
+  my $node = shift;
+  my $lop_ref = shift $node->{cld};
+  my $rop_ref = shift $node->{cld};
 
   emit_token('(');
   compile_node($lop_ref);
 
   while (defined($rop_ref)) {
-    emit_token($node{operator});
+    emit_token($node->{operator});
     compile_node($rop_ref);
 
-    $rop_ref = shift @{$node{cld}};
+    $rop_ref = shift $node->{cld};
   }
 
   emit_token(')');
 }
 
 sub compile_if {
-  my %node = %{shift @_};
-
-  my $cond_ref = shift @{$node{cld}};
-  my $true_ref = shift @{$node{cld}};
-  my $false_ref = shift @{$node{cld}};
+  my $node = shift;
+  my ($cond_ref, $true_ref, $false_ref) = @{$node->{cld}};
 
   emit_keyword('if');
   compile_node($cond_ref);
   compile_node($true_ref);
-  emit_keyword('else') if defined($false_ref);
-  compile_node($false_ref) if defined($false_ref);
+  ( emit_keyword('else'), compile_node($false_ref) ) if defined($false_ref);
 }
 
 sub compile_while {
-  my %node = %{shift @_};
-
-  my $cond_ref = shift @{$node{cld}};
-  my $body_ref = shift @{$node{cld}};
+  my $node = shift;
+  my ($cond_ref, $body_ref) = @{$node->{cld}};
 
   emit_keyword('while');
   compile_node($cond_ref);
@@ -315,9 +241,9 @@ sub compile_while {
 }
 
 sub compile_for {
-  my %node = %{shift @_};
+  my $node = shift;
 
-  my ($init_ref, $cond_ref, $post_ref, $body_ref) = @{$node{cld}};
+  my ($init_ref, $cond_ref, $post_ref, $body_ref) = @{$node->{cld}};
   # TODO: run $post_ref on "continue"
 
   compile_node($init_ref);
@@ -333,11 +259,8 @@ sub compile_for {
 }
 
 sub compile_foreach {
-  my %node = %{shift @_};
-
-  my $iterator_ref = shift @{$node{cld}};
-  my $range_ref = shift @{$node{cld}};
-  my $body_ref = shift @{$node{cld}};
+  my $node = shift;
+  my ($iterator_ref, $range_ref, $body_ref) = @{$node->{cld}};
 
   emit_keyword('for');
   compile_node($iterator_ref);
@@ -360,23 +283,16 @@ sub compile_loopcontrol {
   }
 }
 
-sub compile_fileread {
-  my %node = %{shift @_};
-
-  compile_function_call('__p2p_readline');
-  warn 'todo filedes';
-}
-
 sub compile_body {
-  my %node = %{shift @_};
+  my $node = shift;
 
   $cs{depth}++;
 
   emit_token(':');
 
-  for my $node_ref (@{$node{cld}}) {
+  for my $child (@{$node->{cld}}) {
     emit_statement_begin();
-    compile_node($node_ref);
+    compile_node($child);
   }
 
   $cs{depth}--;
@@ -384,7 +300,7 @@ sub compile_body {
 }
 
 sub compile_program {
-  my %node = %{shift @_};
+  my $node = shift;
 
   # Small hack to get our preamble set up
   open F, '<preamble.py';
@@ -393,7 +309,7 @@ sub compile_program {
   emit_internal_string($preamble);
   emit_statement_begin();
 
-  my @cld = @{$node{cld}};
+  my @cld = @{$node->{cld}};  # Useless but simplifies next few lines
 
   # Get rid of shebang
   if (@cld && ${$cld[0]}{type} eq 'comment' && ${$cld[0]}{value} =~ /^#!/) {
@@ -407,55 +323,52 @@ sub compile_program {
 }
 
 sub compile_node {
-  my %node = %{shift @_};
+  my $node = shift;
 
   $cs{node_depth}++;
 
-  given ($node{type}) {
-    when ('program')          { compile_program         (\%node); }
-    when ('body')             { compile_body            (\%node); }
+  given ($node->{type}) {
+    when ('program')          { compile_program         ($node); }
+    when ('body')             { compile_body            ($node); }
 
-    when ('comment')          { compile_comment         (\%node); }
-    when ('number')           { compile_number          (\%node); }
-    when ('string')           { compile_string          (\%node); }
-    when ('scalar')           { compile_scalar          (\%node); }
-    when ('range')            { compile_range           (\%node); }
+    when ('comment')          { compile_comment         ($node); }
+    when ('number')           { compile_number          ($node); }
+    when ('string')           { compile_string          ($node); }
+    when ('scalar')           { compile_scalar          ($node); }
+    when ('range')            { compile_range           ($node); }
 
-    when ('incdec')           { compile_incdec          (\%node) }
+    when ('incdec')           { compile_incdec          ($node) }
 
-    when ('comma')            { compile_comma           (\%node); }
+    when ('comma')            { compile_comma           ($node); }
 
-    when ('foldl')            { compile_foldl           (\%node); }
-    when ('unary')            { compile_unary           (\%node); }
+    when ('foldl')            { compile_foldl           ($node); }
+    when ('unary')            { compile_unary           ($node); }
 
-    when ('assignment')       { compile_assignment      (\%node); }
-    when ('add_expr')         { compile_binary_op_expr  (\%node); }
-    when ('mul_expr')         { compile_binary_op_expr  (\%node); }
-    when ('logical')          { compile_binary_op_expr  (\%node); }
-    when ('power')            { compile_binary_op_expr  (\%node); }
-    when ('comparison')       { compile_binary_op_expr  (\%node); }
-    when ('comma_sep_expr')   { compile_comma_sep_expr  (\%node); }
+    when ('assignment')       { compile_assignment      ($node); }
+    when ('add_expr')         { compile_binary_op_expr  ($node); }
+    when ('mul_expr')         { compile_binary_op_expr  ($node); }
+    when ('logical')          { compile_binary_op_expr  ($node); }
+    when ('power')            { compile_binary_op_expr  ($node); }
+    when ('comparison')       { compile_binary_op_expr  ($node); }
+    when ('comma_sep_expr')   { compile_comma_sep_expr  ($node); }
 
-    when ('call')             { compile_call            (\%node); }
+    when ('call')             { compile_call            ($node); }
 
-    when ('if_expr')          { compile_if              (\%node); }
-    when ('while_expr')       { compile_while           (\%node); }
-    when ('for_expr')         { compile_for             (\%node); }
-    when ('foreach_expr')     { compile_foreach         (\%node); }
+    when ('if_expr')          { compile_if              ($node); }
+    when ('while_expr')       { compile_while           ($node); }
+    when ('for_expr')         { compile_for             ($node); }
+    when ('foreach_expr')     { compile_foreach         ($node); }
 
-    when ('loop_control')     { compile_loopcontrol     (\%node); }
+    when ('loop_control')     { compile_loopcontrol     ($node); }
 
-    when ('parenthesise')     { compile_parenthesise    (\%node); }
+    when ('parenthesise')     { compile_parenthesise    ($node); }
 
-    when ('file_read')        { compile_fileread        (\%node); }
-
-    # I hate perl at times. More so than the other times when I want to kill it.
-    when ('comma_sep_string_concat') { compile_comma_sep_string_concat(\%node); }
-    when ('stringify')        { compile_stringify       (\%node); }
+    when ('comma_sep_string_concat') { compile_comma_sep_string_concat($node); }
+    when ('stringify')        { compile_stringify       ($node); }
 
     default           {
       print "What are you doing? Got this: ";
-      display(\%node);
+      display($node);
       die;
     }
   }
