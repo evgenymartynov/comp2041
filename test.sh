8888389374566be5d1b23c343adb8781217a42db
Evgeny Martynov <evgeny@epochfail.com>
2013-10-02 06:15:37 +0000

    Add timeouts to test.sh

--- test.sh
+++ test.sh
@@ -2,5 +2,7 @@
 
 for f in examples/*.pl; do
   echo "Doing $f"
-  diff -q <(python <(./perl2python "$f") < /dev/null) <(perl "$f" < /dev/null)
+  diff -q \
+      <(python <(./perl2python "$f") < /dev/null | tail -n 10) \
+      <(timeout -k 2 1 perl "$f" < /dev/null 2>&1 | tail -n 10)
 done
